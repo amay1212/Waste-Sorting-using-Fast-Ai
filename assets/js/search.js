df69b3ae6769238667920b72b0c9b4989c8e2ac4
amay1212 <amay1212@users.noreply.github.com>
2022-05-21 11:55:13 +0000

    deploy: d55f30562eaeeba2d7e60801229b1f72c92b16cf

--- assets/js/search.js
+++ assets/js/search.js
@@ -47,7 +47,7 @@ jtd.onReady = function(ready) {
 
 function initSearch() {
     var request = new XMLHttpRequest();
-    request.open('GET', '/Waste-Sorting-using-Fast-Ai/assets/js/search-data.json', true);
+    request.open('GET', '/amay1212.github.io/assets/js/search-data.json', true);
   
     request.onload = function(){
       if (request.status >= 200 && request.status < 400) {
